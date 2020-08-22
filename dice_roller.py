0593e0546623bbf6decbdd8687e02c67080d0e2e
terryt11 <70047628+terryt11@users.noreply.github.com>
2020-08-22 14:16:32 -0700

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
+import random
+dicerolls = 2
+
 def main():
-  print('You rolled a die')
+  dicesum = 0
+  for i in range(0,dicerolls):
+    roll = random.randint(1, 6)
+    dicesum += roll
+    print(f"You rolled a {roll}")
+  print(f'You have rolled a total of {dicesum}')
   
 if __name__== "__main__":
   main()
