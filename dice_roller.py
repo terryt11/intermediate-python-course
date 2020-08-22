03c579c1d0f92b096410498a24ab1cf41d4551b7
terryt11 <70047628+terryt11@users.noreply.github.com>
2020-08-22 14:29:30 -0700

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,18 @@
 import random
-dicerolls = 2
+dicerolls = int(input("How many dice would you like to roll? "))
+dicesize = int(input("How many sides are the dice? "))
 
 def main():
   dicesum = 0
   for i in range(0,dicerolls):
-    roll = random.randint(1, 6)
+    roll = random.randint(1, dicesize)
     dicesum += roll
-    print(f"You rolled a {roll}")
+    if roll == 1:
+      print(f"You rolled a {roll}, you suck.")
+    elif roll==dicesize:
+      print(f"You rolled a {roll}... I've seen better.")
+    else:
+      print(f"You rolled a {roll}")
   print(f'You have rolled a total of {dicesum}')
   
 if __name__== "__main__":
